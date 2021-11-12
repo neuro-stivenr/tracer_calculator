4c7e0930dc758a5643cd961476189e01d1713c49
Stiven Roytman <stivenr@wsdfk000.ddns.med.umich.edu>
2021-11-12 17:48:28 -0500

    First iteration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const appStyle: any = {
+  InputLine: {
+    marginTop: '20px',
+    marginBottom: '20px'
+  }
+}
+
+interface InputLineProps {
+  placeholder: string;
+  setter: Function;
+}
+
+function InputLine(props:InputLineProps) {
+  const handleChange = (e:any) => {
+    props.setter(e.target.value)
+  }
+  return (
+    <div style={appStyle.InputLine}>
+      <input
+        type='text'
+        placeholder={props.placeholder}
+        onChange={handleChange}
+      />
+    </div>
+  )
+}
+
 function App() {
+  
+  const [patid, setPatid] = useState('')
+  const patidProps: InputLineProps = {
+    placeholder: 'Participant ID',
+    setter: setPatid
+  } 
+
+  const [tracer, setTracer] = useState('')
+  const tracerProps: InputLineProps = {
+    placeholder: 'Tracer',
+    setter: setTracer
+  }
+
+  const [dose, setDose] = useState('')
+  const doseProps: InputLineProps = {
+    placeholder: 'Dose',
+    setter: setDose
+  }
+
+  const handleSubmit = (e:any) => {
+    console.log(`patid: ${patid}`)
+    console.log(`tracer: ${tracer}`)
+    console.log(`dose: ${dose}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <InputLine 
+          {...patidProps}
+        />
+        <InputLine
+          {...tracerProps}
+        />
+        <InputLine
+          {...doseProps}
+        />
+        <input 
+          type='button' 
+          onClick={handleSubmit}
+        />
+      </div>
     </div>
   );
 }
